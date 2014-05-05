d6d0bdeb77beedff3ee7927c612c8bfd9a8972fe
Mhairi <bryanosaur@gmail.com>
2014-05-05 17:51:12 -0400

    piglow example

--- piglow-example.py
+++ piglow-example.py
@@ -14,33 +14,33 @@ CMD_SET_PWM_VALUES = 0x01
 CMD_UPDATE = 0x16
 
 class PiGlow:
-i2c_addr = 0x54 # fixed i2c address of SN3218 ic
-bus = None
+	i2c_addr = 0x54 # fixed i2c address of SN3218 ic
+	bus = None
 
-def __init__(self, i2c_bus=1):
-self.bus = SMBus(i2c_bus)
+	def __init__(self, i2c_bus=1):
+		self.bus = SMBus(i2c_bus)
 
         # first we tell the SN3218 to enable output (turn on)
-self.write_i2c(CMD_ENABLE_OUTPUT, 0x01)
+		self.write_i2c(CMD_ENABLE_OUTPUT, 0x01)
 
         # then we ask it to enable each bank of LEDs (0-5, 6-11, and 12-17)
-self.write_i2c(CMD_ENABLE_LEDS, [0xFF, 0xFF, 0xFF])
+		self.write_i2c(CMD_ENABLE_LEDS, [0xFF, 0xFF, 0xFF])
 
-def update_leds(self, values):
-print "update pwm"
-self.write_i2c(CMD_SET_PWM_VALUES, values)
-self.write_i2c(CMD_UPDATE, 0xFF)
+	def update_leds(self, values):
+		print "update pwm"
+		self.write_i2c(CMD_SET_PWM_VALUES, values)
+		self.write_i2c(CMD_UPDATE, 0xFF)
 
-# a helper that writes the given value or list of values to the SN3218 IC
-# over the i2c protocol
-def write_i2c(self, reg_addr, value):
+	# a helper that writes the given value or list of values to the SN3218 IC
+	# over the i2c protocol
+	def write_i2c(self, reg_addr, value):
         # if a single value is provided then wrap it in a list so we can treat
         # all writes in teh same way
-if not isinstance(value, list):
-value = [value];
+		if not isinstance(value, list):
+			value = [value];
 
         # write the data to the SN3218
-self.bus.write_i2c_block_data(self.i2c_addr, reg_addr, value)
+		self.bus.write_i2c_block_data(self.i2c_addr, reg_addr, value)
 
 # a list of 18 values between 0 - 255 that represent each LED on the PiGlow.
 # to change the LEDs we set the values in this array and then pass it to the
@@ -65,8 +65,7 @@ try:
                 time.sleep(0.1)
                 
 except KeyboardInterrupt:
-# set all the LEDs to "off" when Ctrl+C is pressed before exiting
+	# set all the LEDs to "off" when Ctrl+C is pressed before exiting
         values = [0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00]
         piglow.update_leds(values)
-
 
