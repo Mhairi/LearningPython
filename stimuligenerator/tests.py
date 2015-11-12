18329e5eae94aa309b85f54b2dfca3ade4da2508
Bryan <bryan@ohgodscience.com>
2015-11-12 17:21:43 -0500

    Templates for testing and app

---
+++ stimuligenerator/tests.py
@@ -0,0 +1,15 @@
+from stimuligenerator import GenerateSelf, GeneratePartner, GenerateGroup
+from stimuligenerator import SelfvsPartner, SelfvsGroup, PartnervsGroup
+
+#These 4 classes simply test if the first object returns what it should 
+#(e.g., that the self-interest stimuli give more to the self than the
+#other person).
+
+class SelfInterestTests(SelfInterestStimuli):
+    def SelfMoreThanOther(self):
+        
+    
+class PartnerInterestTests(PartnerInterestStimuli):
+
+class GroupInterestTests(GroupInterestStimuli):
+
