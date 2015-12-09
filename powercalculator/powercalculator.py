1e6d351a90b71a1284f721b84292742d69439f62
Bryan <bryan@ohgodscience.com>
2015-12-09 11:00:46 -0500

    More initialization

--- powercalculator/powercalculator.py
+++ powercalculator/powercalculator.py
@@ -2,16 +2,21 @@
 from scipy.stats import f,t #this imports the f and t distribution functions
 
 def anovapower(df1, df2, alpha): #we take the n, the alpha and the effect size
-	alpha = 1-alpha #to convert to the cumulative partition
-	#df2 is related to sample size
+    alpha = 1-alpha #to convert to the cumulative partition
+    #df2 is related to sample size
 	
-	#
-	return f.ppf (alpha, df1, df2)
+    return f.ppf (alpha, df1, df2)
 	
 def samplesize(arrayofmeans, alpha): #we'll take some standardized means, and an alpha level
-	#we want to return the sample size needed
-	alpha = 1-alpha
-	meandifference = arrayofmeans[1]-arrayofmeans[0]
-	n = 30
-	if t.cdf(meandifference,n) > alpha:
-		return 
+    #we want to return the sample size needed
+    alpha = (alpha/2) #two tailed
+    meandifference = arrayofmeans[1]-arrayofmeans[0]
+    steps = 10
+    n = 30
+    counter = 0
+    sigtvalue = t.pdf(meandifference, n)
+    while sigtvalue > alpha:
+        sigtvalue = t.pdf(meandifference, n)
+        n += 1
+    
+    return n
