4c57cfbe39f178bcc80afef86ec9a8ed5399e64f
Bryan <bryan@ohgodscience.com>
2016-02-09 14:30:24 -0500

    Initial commit

---
+++ Exercises/DigitFactorials.py
@@ -0,0 +1,5 @@
+#Numbers from 1-9 and their corresponding factorial [1,2,6,24,120,720,5040,40320,362880]
+#Array of digit length? [1,1,1,2,3,3,4,5,6]
+#We can use this as min length of string (e.g., if 9 is in the string, the string cannot shorter than 6 digits)
+#Test all 2 digit integers
+#First pass print a list of numbers that pass the test
